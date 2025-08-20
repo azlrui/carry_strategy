0e2687a54df470fd762712afe5e72d04b6b75277
azlrui <rui.azevedoleitao@gmail.com>
2025-08-20 22:25:38 +0200

    started to analyze fx data

---
+++ src/analyze_fx.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def clean_dataset(df:pd.DataFrame, filter_start: str, filter_end: str) -> pd.DataFrame :
+    start, end = filter_start, filter_end
+
+    df = df.loc[start:end]
+
+    cols_with_nan = df.columns[df.isna().any()]
+    df = df.drop(columns=cols_with_nan)
+    
+    return df
+
+def main():
+    # Upload FX_DATASET
+    file = "data/fx_data_USD_2001-01-01_2025-01-01_1mo.csv"
+    start, end = "2006-06-30", "2025-01-01"
+
+    fx_data = pd.read_csv(file, index_col='Date', parse_dates=True)
+    fx_data = clean_dataset(fx_data, start, end)
+    breakpoint()
+
+if __name__ == "__main__":
+    raise SystemExit(main())
